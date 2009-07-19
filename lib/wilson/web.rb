9776a81f4b525b96221c7c89a040bdff353aa0bc
Florian Gilcher <flo@andersground.net>
2009-07-19 10:58:53 +0900

    Added wilson/web - a web publisher for Sinatra routes.
    Made Wilson not load parsers on 1.8.6, as they are 1.9-only at the moment.

---
+++ lib/wilson/web.rb
@@ -0,0 +1,38 @@
+require 'wilson'
+require 'json/pure'
+
+module Wilson
+  module Web
+    def self.included(base)
+      
+      base.get '/wilson' do
+        content_type 'text/plain'
+        
+        state = JSON::Pure::Generator::State.new(
+          :indent => '  ',
+          :object_nl => "\n",
+          :array_nl => "\n"
+        )
+        
+        YARD::Registry.load
+        code_obj = YARD::Registry[base.name]
+        code_obj[:routes].map do |route|
+          hsh = {
+            :route => route.route,
+            :description => route.docstring
+          }
+          hsh[:properties] = {}
+          
+          route.tags.each do |tag| 
+            hsh[:properties][tag.tag_name] = { 
+                                              :types => tag.types, 
+                                              :desc  => tag.text
+                                            }
+          end
+          
+          hsh
+        end.to_json(state)
+      end
+    end
+  end
+end
