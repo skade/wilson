66132687984ac3c059615551d1b9452130a818e6
Florian Gilcher <flo@andersground.net>
2010-01-27 17:47:53 +0100

    fixes tests for yard 0.5.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,5 @@ require 'test/unit'
 def parse_file(file, thisfile = __FILE__, log_level = log.level)
   YARD::Registry.clear
   path = File.join(File.dirname(thisfile), 'examples', file.to_s + '.rb.txt')
-  YARD::Parser::SourceParser.parse(path, log_level)
+  YARD::Parser::SourceParser.parse([path], [], log_level)
 end
